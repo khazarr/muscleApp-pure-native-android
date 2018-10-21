92fb9093d64f8190b0a626c7b2d043c4a64c752c
Karol Swieca <swieca.karol@gmail.com>
2018-10-21 15:33:54 +0200

    Basic TImer

--- app/src/main/java/pl/swiecajs/muscleapp/Timer.java
+++ app/src/main/java/pl/swiecajs/muscleapp/Timer.java
@@ -1,6 +1,7 @@
 package pl.swiecajs.muscleapp;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,53 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import pl.swiecajs.muscleapp.util.PrefUtil;
 
 public class Timer extends AppCompatActivity {
+
+    public enum TimerState {
+        STOPPED(0),
+        PAUSED(1),
+        RUNNING(2);
+
+        private int id;
+
+        TimerState(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public static TimerState getValue(int id) {
+            for(TimerState e: TimerState.values()) {
+                if(e.id == id) {
+                    return e;
+                }
+            }
+            return null;// not found
+        }
+    }
+
+
+
+    private CountDownTimer timer;
+    private Long timerLengthSeconds = 0L;
+    private Long secondsRemaining = 0L;
+    private TimerState timerState = TimerState.STOPPED;
+
+
+    private ProgressBar progressBar;
+    private TextView countdownText;
+    private FloatingActionButton fab_start;
+    private FloatingActionButton fab_stop;
+    private FloatingActionButton fab_pause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +63,164 @@ public class Timer extends AppCompatActivity {
         setContentView(R.layout.activity_timer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setIcon(R.drawable.ic_timer);
+        getSupportActionBar().setTitle("   MuscleApp");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        progressBar = findViewById(R.id.progress_countdown);
+        countdownText = findViewById(R.id.textView_countdown);
+        fab_start = findViewById(R.id.fab_start);
+        fab_stop = findViewById(R.id.fab_stop);
+        fab_pause = findViewById(R.id.fab_pause);
+
+        fab_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startTimer();
+                timerState = TimerState.RUNNING;
+                updateButtons();
             }
         });
+
+        fab_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.cancel();
+                timerState = TimerState.PAUSED;
+                updateButtons();
+            }
+        });
+
+        fab_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.cancel();
+                onTimerFinished();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        initTimer();
+
+        // TODO: remove background timer, hide notification
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(timerState == TimerState.RUNNING) {
+            timer.cancel();
+            //TODO: start background timer and show notification
+        } else if(timerState == TimerState.PAUSED) {
+            //TODO: show notification
+        }
+
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, this);
+        PrefUtil.setSecondsRemaining(secondsRemaining, this);
+        PrefUtil.setTimerState(timerState.getId(),this);
+
+
+    }
+
+    private void initTimer() {
+        timerState = PrefUtil.getTimerState(this);
+
+        if (timerState == TimerState.STOPPED) {
+            setNewTimerLength();
+        } else {
+            setPreviousTimerLength();
+        }
+
+        secondsRemaining = (timerState == TimerState.RUNNING || timerState == TimerState.PAUSED)
+                ? PrefUtil.getSecondsRemaining(this)
+                : timerLengthSeconds;
+
+        //TODO: change secondsRemaining according to where the background timer stopped
+
+        //resume where we left off
+        if (timerState == TimerState.RUNNING) {
+            startTimer();
+        }
+
+        updateButtons();
+        updateCountdownUI();
+    }
+
+    private void onTimerFinished() {
+        timerState = TimerState.STOPPED;
+
+        setNewTimerLength();
+
+
+        progressBar.setProgress(0);
+
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, this);
+        secondsRemaining = timerLengthSeconds;
+
+        updateButtons();
+        updateCountdownUI();
+    }
+
+    private void startTimer() {
+        timerState = TimerState.RUNNING;
+        timer = new CountDownTimer(secondsRemaining * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                secondsRemaining = millisUntilFinished / 1000;
+                updateCountdownUI();
+            }
+
+            @Override
+            public void onFinish() {
+                onTimerFinished();
+            }
+        }.start();
+    }
+
+    private void setNewTimerLength() {
+        int lengthInMinutes = PrefUtil.getTimerLength(this);
+        timerLengthSeconds = lengthInMinutes * 60L;
+        progressBar.setMax(Math.toIntExact(timerLengthSeconds));
+    }
+
+    private void setPreviousTimerLength() {
+        timerLengthSeconds = PrefUtil.getPreviousTImerLengthSeconds(this);
+        progressBar.setMax(Math.toIntExact(timerLengthSeconds));
+    }
+
+    private void updateCountdownUI() {
+        Long minutesUntilFinished = secondsRemaining / 60;
+        Long secondsUntilFinished = secondsRemaining - minutesUntilFinished * 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutesUntilFinished, secondsUntilFinished);
+        countdownText.setText(timeLeftFormatted);
+
+        progressBar.setProgress((timerLengthSeconds.intValue()-secondsRemaining.intValue()));
+    }
+
+    private void updateButtons() {
+        switch (timerState) {
+            case RUNNING:
+                fab_start.setEnabled(false);
+                fab_pause.setEnabled(true);
+                fab_stop.setEnabled(true);
+                break;
+            case STOPPED:
+                fab_start.setEnabled(true);
+                fab_pause.setEnabled(false);
+                fab_stop.setEnabled(false);
+                break;
+            case PAUSED:
+                fab_start.setEnabled(true);
+                fab_pause.setEnabled(false);
+                fab_stop.setEnabled(true);
+                break;
+        }
     }
 
     @Override
---
+++ app/src/main/java/pl/swiecajs/muscleapp/util/PrefUtil.java
@@ -0,0 +1,55 @@
+package pl.swiecajs.muscleapp.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import pl.swiecajs.muscleapp.Timer;
+
+public class PrefUtil {
+
+    public static int getTimerLength (Context context) {
+        // placeholder
+        return 1;
+    }
+
+    private static final String PREVIOUS_TIMER_LENGTH_SECONDS_ID = "pl.swiecajs.muscleapp.previous_timer_length";
+
+    public static Long getPreviousTImerLengthSeconds(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID,0);
+    }
+
+    public static void setPreviousTimerLengthSeconds(Long seconds, Context context) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds);
+        editor.apply();
+    }
+
+    private static final String TIMER_STATE_ID = "pl.swiecajs.muscleapp.timer_state";
+
+    public static Timer.TimerState getTimerState(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        int timerStateId = preferences.getInt(TIMER_STATE_ID, Timer.TimerState.STOPPED.getId());
+        return Timer.TimerState.getValue(timerStateId);
+    }
+
+    public static void setTimerState(int timerStateId, Context context) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putInt(TIMER_STATE_ID, timerStateId);
+        editor.apply();
+    }
+
+    private static final String SECONDS_REMAININD_ID = "pl.swiecajs.muscleapp.seconds_remaining";
+
+    public static Long getSecondsRemaining(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getLong(SECONDS_REMAININD_ID,0);
+    }
+
+    public static void setSecondsRemaining(Long seconds, Context context) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putLong(SECONDS_REMAININD_ID, seconds);
+        editor.apply();
+    }
+}
